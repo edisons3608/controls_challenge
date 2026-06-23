1230c1985cd55fd9572d6e09a0be04a889fc8977
Edison Sun <edisonsun2028@u.northwestern.edu>
2026-06-22 22:27:03 -0400

    Create controller.py

---
+++ controllers/controller.py
@@ -0,0 +1,11 @@
+from . import BaseController
+import numpy as np
+
+class Controller(BaseController):
+
+  def __init__(self,):
+    pass
+
+  def update(self, target_lataccel, current_lataccel, state, future_plan):
+    pass
+
